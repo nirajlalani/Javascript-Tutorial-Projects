32ce9b0aee79bb7042880dd61708cfab74926fd1
nirajlalani <nirajlalani01@gmail.com>
2024-09-03 20:02:02 +0530

    index.js created

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// 1) Ways to print in Javascript
+console.log("Hello World")
+document.write("This is a document write")
+alert("This is an alert")
+
+// 2) JS Console API
+console.log("Hello World",4+6,"Another Log")
+console.warn("This is a warning")
+console.error("This is an error")
