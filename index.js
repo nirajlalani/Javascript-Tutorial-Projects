c60fd1d73f855a72354aecf51b58898ebaaeb182
nirajlalani <nirajlalani01@gmail.com>
2024-09-07 05:44:08 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
 
 // 1) Ways to print in Javascript
-console.log("Hello World")
-document.write("This is a document write")
-alert("This is an alert")
+// console.log("Hello World");
+// document.write("This is a document write");
+// alert("This is an alert");
 
 // 2) JS Console API
-console.log("Hello World",4+6,"Another Log")
-console.warn("This is a warning")
-console.error("This is an error")
+// console.log("Hello World",4+6,"Another Log");
+// console.warn("This is a warning");
+// console.error("This is an error");
+
+// 3) JS Variables 
+// var num1 = 35;
+// var num2 = 89;
+// console.log(num1+num2);
+
+// 4) Data Types in JS
+
+// String
+// var str1 = "This is a string";
+// var str2 = "This is also a string";
+
+// Numbers
+// var num1 = 45;
+// var num2 = 99;
+
+// Objects
+// var marks = {
+//     ravi : 45,
+//     dev : 69
+// }
+
+// Boolean
+// var a = true;
+// var b = false;
+
+// var a = undefined;
+// var b = null;
+
+// var arr = [56,24,51,"Ved",42]
+// console.log(arr)
+
+// 5) Operators in JS
+
+// Arithmetic operators
+// var a = 60;
+// var b = 78;
+// console.log("The value of a+b is",a+b)
+// console.log("The value of a-b is",a-b)
+// console.log("The value of a*b is",a*b)
+// console.log("The value of a/b is",a/b)
+
+//Assignment Operators
