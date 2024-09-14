701f2201da95486883c1717a5bd0a692111217dc
nirajlalani <nirajlalani01@gmail.com>
2024-09-15 05:24:39 +0530

    index.js updated

--- index.js
+++ index.js
@@ -107,11 +107,51 @@
 //     console.log("You are not an adult");
 // }
 
-var arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(arr);
-for(var i=0;i<arr.length;i++){
-    if(i==2){
-        break;
-    }
-    console.log(arr[i])
-}
+// var arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(arr);
+// for(var i=0;i<arr.length;i++){
+//     if(i==2){
+//         break;
+//     }
+//     console.log(arr[i])
+// }
+
+// let j = 6
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++
+// }
+
+// do{
+//     console.log(arr[j]);
+//     j++;
+// }while(j<=arr.length);
+
+
+// let arr2 = ["abcd","efgh",45,null,true];
+
+// ARRAY METHODS
+// console.log(arr2.length);
+// arr2.pop();
+// arr2.push("ijkl");
+// arr2.shift();
+// console.log(arr2)
+// arr2.toString();
+// arr2.sort();
+// console.log(arr2);
+
+// STRING METHODS
+// let str = "Niraj null true false";
+// console.log(str.length);
+// console.log(str.indexOf("true"));
+// console.log(str.lastIndexOf("true"));
+// console.log(str.slice(0,4));
+// console.log(str.replace("Niraj","Nirajjj"))
+
+// let mydate = new Date();
+// console.log(mydate);
+// console.log(mydate.getTime());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getDay());
+// console.log(mydate.getMinutes());
+// console.log(mydate.getHours());
